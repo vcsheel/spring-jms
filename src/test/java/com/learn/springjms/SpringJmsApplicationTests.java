ec60529b0cd8bac62226ef6f207d5a3c8d061976
vcsheel <viveksheel96@gmail.com>
2020-06-19 15:26:44 +0530

    initial commit

---
+++ src/test/java/com/learn/springjms/SpringJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
