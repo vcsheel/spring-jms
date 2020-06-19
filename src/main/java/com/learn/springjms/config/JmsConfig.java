4b588953030322b3bf41209f3590a94865b612b0
vcsheel <viveksheel96@gmail.com>
2020-06-19 15:56:52 +0530

    Added MessageConverter

---
+++ src/main/java/com/learn/springjms/config/JmsConfig.java
@@ -0,0 +1,17 @@
+package com.learn.springjms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
